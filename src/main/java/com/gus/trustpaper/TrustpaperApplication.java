8f5bd0eacbeb7dbe01b3998ce58530ce7a20d146
Gustavo <narugu@hotmail.com>
2025-07-20 22:16:17 -0300

    subida do projeto

---
+++ src/main/java/com/gus/trustpaper/TrustpaperApplication.java
@@ -0,0 +1,13 @@
+package com.gus.trustpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustpaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustpaperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gus/trustpaper/TrustpaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gus.trustpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustpaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
